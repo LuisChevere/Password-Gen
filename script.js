fdf7492153000d1a3afe7bae0618c2efddd5d159
Luis <Hairchevere@gmail.com>
2022-10-25 22:01:52 -0500

    added functions to script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialCharacter = "!@#$%^&*()"
 
 // Write password to the #password input
 function writePassword() {
@@ -7,11 +8,25 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  passwordText.value = "password"
 
 }
+function generatePassword() {
+  var numberCharacters = prompt("How many characters would you like password to be?")
+  numberCharacters = parseInt(numberCharacters)
+  console.log(numberCharacters)
+  console.log(typeof numberCharacters)
+  if(isNaN(numberCharacters)) {
+    alert ("Password must be a number")
+  }
+  if(numberCharacters < 8 || numberCharacters >128){
+    alert ("Number must be between 18-128")
+  }
+  if(numberCharacters >8) { 
+    confirm("should this include special characters?")
+    if(true)
+  }
+}
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", function(){ alert("password"); });
-
-
+generateBtn.addEventListener("click", writePassword);
